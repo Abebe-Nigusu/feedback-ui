8f7dbce8ac79adad83b912304590ccdc4996f039
Abebe-Nigusu <92810261+Abebe-Nigusu@users.noreply.github.com>
2023-06-15 17:12:30 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const ratingElements = document.querySelectorAll(".rating");
+
+const btnElement = document.getElementById("btn")
+
+const containerElement = document.getElementById("container")
+
+let selectedRating = "";
+
+ratingElements.forEach((ratingElement) => {
+    ratingElement.addEventListener("click", (event) => {
+        removeActive();
+        selectedRating = event.target.innerText || event.target.parentNode.addinnerText;
+        event.target.classList.add("active");
+        event.target.parentNode.classList.add("active");
+
+    });
+});
+
+btnElement.addEventListener("click", () => {
+    if (selectedRating !== "") {
+        containerElement.innerHTML =
+            `
+            <strong>Thank you!</strong>
+            <br>
+            <br>
+            <strong>Feedback: ${selectedRating}</strong>
+            <p>We'll use your feedback to improve our customer support.</p>
+           `;
+            
+            
+    }
+} )
+
+function removeActive() {
+    ratingElements.forEach((ratingElement) => {
+        ratingElement.classList.remove("active");
+    })
+}
